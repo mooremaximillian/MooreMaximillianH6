c28db72315ab9101995587919c6442b9c69b14c0
Maximillian Moore <mooremaximillian@gmail.com>
2017-05-07 23:48:59 -0400

    Finishes the reader input

--- BST.java
+++ BST.java
@@ -1,96 +1,124 @@
-public class BST
-{
-  
-  public BSTNode root;
-  
-  
-  public BST()
-  {
-    root = null;
-  }
-  
-  public boolean isEmpty()
-  {
-    return root == null;
-  }
-  
-  public BSTNode getRoot()
-  {
-    return root;
-  }
-  
-  public void insert(int data)
-  {
-    insert(root, data);
-  }
-   private void insert(BSTNode node, int data)
-     {
-       
-      if(data <= node.getData())
-      {
-        if(node.getLeft() != null)
-        {
-        insert(node.getLeft(),data);
-        }
-      else
-      {
-        node.setLeft(new BSTNode(data));
-      }
-      }
-      else
-      {
-        if(node.getRight() != null)
-        {
-          insert(node.getRight(), data);
-        }
-        else
-        {
-          node.setRight(new BSTNode(data));
-        }
-      }
-     }
-   
-   public void search(int data)
-  {
-    search(root, data);
-  }
-   
-   public BSTNode search(BSTNode root, int data)
-{
+public class BST {
+  // properties
+  public BST parent;
+  public BST left;
+  public BST right;
+  public int data;
     
-    if (root==null || root.getData()==data)
-        return root;
-    
+  // constructor
+  public BST() {
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+    this.data = 0;
+  }
+  
+  public BST(int data) {
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+    this.data = data;
+  }
+  
  
-    
-    if (root.getData() > data)
-        return search(root.left, data);
+  
+  public boolean hasLeft() {
+    return !(this.left == null);
+  }
+  
+  public boolean hasRight() {
+    return !(this.right == null);
+  }
+  
+  public boolean hasParent() {
+    return !(this.parent == null);
+  }
+  
+  public void add(int data) {
+    this.add(new BST(data));
+  }
+  
  
+  public void add(BST subtree) {
+    if (subtree != null) {
+      if (subtree.data < this.data) {
+        if (this.hasLeft())
+          this.left.add(subtree);
+        else {
+          this.left = subtree;
+          subtree.parent = this;
+        }
+      } 
+      else
+        if (this.hasRight())
+          this.right.add(subtree);
+        else {
+          this.right = subtree;
+          subtree.parent = this;
+      }
+    }
+  }
+  
+  public void print() {
+    System.out.print(this.data + " ");
+  }
+  
+  public void inOrder() {
+    if (this.left != null)
+      this.left.inOrder();
+    this.print();
+    if (this.right != null)
+      this.right.inOrder();
     
-    return search(root.right, data);
-      
-    
-}
-   
-     
-   
-       
-   
-   public void inorder()
-     {
-         inorder(root);
-     }
-     private void inorder(BSTNode r)
-     {
-       if(r != null)
-       {
-         inorder(r.getLeft());
-         System.out.println(r.getData());
-         inorder(r.getRight());
-       }
-     }
-     
-     public void setRoot(int data){
-         root = new BSTNode(data);
-     }
+  }
+  
+  public BST find (int data) {
+  if(this.data == data)
+    return this;
+  else if(data < this.data && hasLeft() )
+    return left.find(data);
+  else if(data > this.data && hasRight() )
+    return right.find(data);
+  else
+    return null;
+  }
+  
+  public BST delete(int data) {
+    BST tmp = this.find(data);
+    if (tmp == null)
+      return null;
+    if (tmp.parent == null){
+      tmp.left.add(tmp.right);
+      tmp.data = tmp.left.data;
+      tmp.right = tmp.left.right;
+      tmp.left.right.parent = tmp;
+      tmp.left = tmp.left.left;
+      tmp.left.parent = tmp;
+    }
+    else {
+      if (tmp.parent.left.data == data)
+        tmp.parent.left = null;
+      else
+        tmp.parent.right = null;
+      this.add(tmp.left);
+      this.add(tmp.right);
+      tmp.parent = null;
+      tmp.left = null;
+      tmp.right = null;
+    }
+    return(tmp);
+  }
+  
+  public int depth() {
+    int leftDepth = 0;
+    int rightDepth = 0;
+    if (this.hasLeft())
+      leftDepth = left.depth();
+    if (this.hasRight())
+      rightDepth = right.depth();
+    if (leftDepth > rightDepth)
+      return leftDepth + 1;
+    else
+      return rightDepth + 1;
+  }
 }
--- Main.java
+++ Main.java
@@ -1,18 +1,56 @@
+import java.util.Scanner;
+import java.io.Reader;
+import java.io.FileReader;
+
 public class Main
 {
  public static void main(String[] args) {
         BST bst = new BST();  
-        bst.setRoot(10);
+        
         System.out.println("Binary Tree Example");
-        System.out.println("Building tree with root data " + bst.getRoot().getData());
-        bst.insert(6);
-        bst.insert(8);
-        bst.insert(7);
-        bst.insert(3);
-        bst.insert(9);
+        String filename = "data.txt";
+        bst.add(6);
+        bst.add(8);
+        bst.add(7);
+        bst.add(3);
+        bst.add(9);
+        bst.add(5);
+        bst.find(7).print();
+        bst.delete(7);
+        bst.depth();
         System.out.println("Traversing tree in order");
-        bst.inorder();
+        bst.inOrder();
         System.out.println();
+        
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+System.out.println("Enter a number: ");
+int n = reader.nextInt();
+BST bst1 = new BST();  
+
+bst1.add(n);
+bst1.print();
+     
+
+
+        
+
+        
+        
+        try
+    {
+      Scanner scanner = new Scanner(new FileReader(filename));
+
+      while (scanner.hasNext())
+      {
+        
+      }
+      scanner.close();
+    }
+    catch (Exception e)
+    {
+      System.err.format("Exception occurred trying to read '%s'.", filename);
+      e.printStackTrace();
+    }
     } 
   
 }
